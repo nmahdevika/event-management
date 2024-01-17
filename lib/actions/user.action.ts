141092cebf6522ba2e446ffd6eb1461a9e34d278
Novry Mahdevika <novry.mahdevika@neuron.co.id>
2024-01-17 15:59:19 +0700

    Update to latest

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,90 @@
+"use server";
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../mongodb/database";
+import User from "../mongodb/database/models/user.model";
+import Event from "../mongodb/database/models/event.model";
+import { revalidatePath } from "next/cache";
+import Order from "../mongodb/database/models/order.model";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) {
+      throw new Error("User update failed");
+    }
+
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const deleteUser = async (clerkId: string) => {
+  try {
+    await connectToDatabase();
+
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error("User not found");
+    }
+
+    // Unlink relationships
+    await Promise.all([
+      // Update the 'events' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
+    ]);
+
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return JSON.parse(JSON.stringify(deletedUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getUserById = async (userId: string) => {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      // User not found
+      return null;
+    }
+
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+};
